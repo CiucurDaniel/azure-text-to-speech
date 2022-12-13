a92c0e9eeeabf8918c9a951ac6fffa14175fcc11
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-12-13 13:20:28 +0200

    Wrap code in a function

--- tts_output_in_file.py
+++ tts_output_in_file.py
@@ -1,17 +1,25 @@
 import azure.cognitiveservices.speech as speechsdk
 
 
-speech_config = speechsdk.SpeechConfig(subscription="X", region="germanywestcentral")
-speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm)
-synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+def make_tts():
+    print("INFO: Function reached")
+    speech_config = speechsdk.SpeechConfig(subscription="X", region="germanywestcentral")
+    speech_config.set_speech_synthesis_output_format(speechsdk.SpeechSynthesisOutputFormat.Riff24Khz16BitMonoPcm)
+    synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=None)
 
-result = synthesizer.speak_text_async("Hello").get()
-stream = speechsdk.AudioDataStream(result)
-stream.save_to_wav_file("file.wav")
+    print("INFO: Config done")
+    #result = synthesizer.speak_text_async("Hello").get()
+    result = synthesizer.speak_text("Hello")
 
-if stream.status == stream.status.Canceled:
-    print("an error happened, stream got canceled")
-if stream.status == stream.status.NoData:
-    print("no data inside audio file") 
-if stream.status == stream.status.AllData:
-    print("it was a success")
+    stream = speechsdk.AudioDataStream(result)
+    stream.save_to_wav_file("file4.wav")
+
+    print("INFO: Stream going")
+    if stream.status == stream.status.Canceled:
+        return "an error happened, stream got canceled"
+    if stream.status == stream.status.NoData:
+        return "no data inside audio file" 
+    if stream.status == stream.status.AllData:
+        return "it was a success"
+
+# make_tts()
